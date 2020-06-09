8fd272f1909e3d0b5ebe109fec78021f9722bc0c
Tanya Hodges <tanyac52003@gmail.com>
2020-06-09 21:36:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -7,23 +7,5 @@ class Post
     @@all << self 
   end 
   
-  def self.all 
-    @@all 
-  end 
-  
-  def title 
-    self.title 
-  end 
-  
-  def author 
-    post.author = self 
-  end 
-  
-  def author_name
-    if self.author 
-      self.author.title 
-    else 
-      nil 
-    end 
-  end 
+
 end 
